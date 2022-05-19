65e8ae52157ac2184dafea29a81d82bdd56ab91e
Chase Childers <chasechilders12@gmail.com>
2022-05-19 10:01:17 -0400

    added home page

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const http = require("http");
 const express = require("express");
 const data = require("./data");
-
+const { characters, episodes } = require("./data");
 const hostname = "127.0.0.1";
 const port = 3000;
-
 const app = express();
-
 const server = http.createServer(app);
 
 app.use(express.static("./public"));
